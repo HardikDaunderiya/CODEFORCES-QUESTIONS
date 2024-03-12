f7830e2838244b3a1a3814d4e831e2199eb9588a
Hardik Daunderiya <daunderiyahardik@gmail.com>
2024-03-12 12:17:29 +0530

    ROUND 933

---
+++ B_Rudolf_and_121.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <numeric>
+
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define fill(a, x) memset(a, x, sizeof(a))
+#define MOD 1000000007
+#define INF 1000000000
+#define PI 3.1415926535897932384626433832795
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+void solve() {
+    // Your code for each test case here
+    ll n;
+    cin>>n;
+    ll arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-2;i++){
+        if(arr[i]==0) continue;
+        else if(arr[i]<0) {
+            cout<<"NO"<<endl;
+            return;
+        }
+        else{
+            arr[i+1]-=2*arr[i];
+            arr[i+2]-=arr[i];
+        }
+    }
+    if(arr[n-1]==0 && arr[n-2]==0){
+        cout<<"YES"<<endl;
+    }
+    else {
+        cout<<"NO"<<endl;
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // Number of test cases
+    int t;
+    cin >> t;
+    
+    for (int tc = 1; tc <= t; ++tc) {
+        solve();
+    }
+
+    return 0;
+}
